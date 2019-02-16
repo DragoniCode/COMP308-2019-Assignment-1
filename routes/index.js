ef4600992c148c3510c6f9751c6e485f09c5c151
DragoniCode <g7angel_hunter@hotmail.com>
2019-02-16 11:06:50 -0500

    added logos & favicons

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,8 @@ router.get('/about', function(req, res, next) {
 });
 
 /* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
 });
 
 /* GET Services page. */
