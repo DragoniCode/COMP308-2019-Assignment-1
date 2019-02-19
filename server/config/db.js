ef9650a4285e7ea1b4e0ae65c11faccec9d69259
DragoniCode <g7angel_hunter@hotmail.com>
2019-02-18 23:40:57 -0500

    cloud configuration

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "URI" : "mongodb://localhost/test"
-    // "URI" : "mongodb/userName , password connectionString"
+    // "URI" : "mongodb://localhost/test"
+    "URI": "mongodb://Gabe:g7654321@ds058548.mlab.com:58548/comp308-2019-assignment-1"
 }
